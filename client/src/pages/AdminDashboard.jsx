98d756aed883657471de6e1a1907e9788c5560b4
nikilasilva <118242917+nikilasilva@users.noreply.github.com>
2024-09-03 12:42:41 +0530

    merge admin dashboard

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -14,33 +14,48 @@ const AdminDashboard = () => {
 
   return (
     <div className="p-6">
-      <div className="flex space-x-4 mb-6">
-        <button
-          onClick={() => handleTabChange('categories')}
-          className={`px-4 py-2 rounded ${activeTab === 'categories' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
+      <div className="flex flex-wrap justify-between my-4">
+      <div className="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/5">
+        <div
+            className={`relative block py-4 rounded-lg shadow-md hover:shadow-lg hover:cursor-pointer 
+                        ${activeTab === 'categories' ? 'bg-[#0F766E] text-white border-0' : 'bg-white/80 hover:bg-white/85 border-2'}`}
+            onClick={() => handleTabChange('categories')}
         >
-          Category List
-        </button>
-        <button
-          onClick={() => handleTabChange('types')}
-          className={`px-4 py-2 rounded ${activeTab === 'types' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
-        >
-          Type List
-        </button>
-        <button
-          onClick={() => handleTabChange('users')}
-          className={`px-4 py-2 rounded ${activeTab === 'users' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
-        >
-          User List
-        </button>
-        <button
-          onClick={() => handleTabChange('services')}
-          className={`px-4 py-2 rounded ${activeTab === 'services' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
-        >
-          Service List
-        </button>
-        
+            <h3 className="text-xl text-center font-semibold mb-2">Category List</h3>
+        </div>
       </div>
+
+      <div className="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/5">
+          <div
+              className={`relative block py-4 rounded-lg shadow-md hover:shadow-lg hover:cursor-pointer 
+                          ${activeTab === 'types' ? 'bg-[#0F766E] text-white border-0' : 'bg-white/80 hover:bg-white/85 border-2'}`}
+              onClick={() => handleTabChange('types')}
+          >
+              <h3 className="text-xl text-center font-semibold mb-2">Type List</h3>
+          </div>
+      </div>
+
+        <div className="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/5">
+            <div
+                className={`relative block py-4 rounded-lg shadow-md hover:shadow-lg hover:cursor-pointer 
+                            ${activeTab === 'users' ? 'bg-[#0F766E] text-white border-0' : 'bg-white/80 hover:bg-white/85 border-2'}`}
+                onClick={() => handleTabChange('users')}
+            >
+                <h3 className="text-xl text-center font-semibold mb-2">User List</h3>
+            </div>
+        </div>
+
+        <div className="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/5">
+            <div
+                className={`relative block py-4 rounded-lg shadow-md hover:shadow-lg hover:cursor-pointer 
+                            ${activeTab === 'services' ? 'bg-[#0F766E] text-white border-0' : 'bg-white/80 hover:bg-white/85 border-2'}`}
+                onClick={() => handleTabChange('services')}
+            >
+                <h3 className="text-xl text-center font-semibold mb-2">Service List</h3>
+            </div>
+        </div>
+      </div>
+
 
       {/* Render components based on active tab */}
       {activeTab === 'categories' && <CategoryList />}
